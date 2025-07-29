73a8cee5df8711f227464e7b459e62198c273c91
大野秀征 <cbe63010@gmail.com>
2025-07-29 19:17:18 +0900

    fix: 手動サイトマップを作成し、Coming Soonページを除外

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,10 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.munology.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap({
+    filter: (page) => {
+      // Coming Soonページを除外
+      return !page.includes('coming-soon');
+    }
+  })]
 });
